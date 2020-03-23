32e2ba74bf40d425e0db6f86a5f95c5931442176
robin <robin@shroomlife.de>
2020-03-23 20:20:28 +0100

    added eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  'env': {
+    'commonjs': true,
+    'es6': true,
+    'node': true,
+    'jest': true
+  },
+  'extends': 'eslint:recommended',
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly'
+  },
+  'parserOptions': {
+    'ecmaVersion': 2018
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'no-console': 0
+  }
+}
